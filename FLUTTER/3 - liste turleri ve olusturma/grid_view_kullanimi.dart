33af3f533cbfda9b7ee2650142b0baec894b9115
Melike <melikegmstkn@gmail.com>
2021-07-04 22:06:18 +0300

    gridView count/extent/builder

--- FLUTTER/3 - liste turleri ve olusturma/grid_view_kullanimi.dart
+++ FLUTTER/3 - liste turleri ve olusturma/grid_view_kullanimi.dart
@@ -69,6 +69,60 @@ class GridViewOrnek extends StatelessWidget {
     );
   }
 
+  GridView buildGridView_builder() {
+    return GridView.builder(
+      itemCount: 100,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3), //eksendeki delegate sayısı
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.deepPurple, width: 5),
+              //borderRadius: new BorderRadius.all(new Radius.circular(5.0)),
+              boxShadow: [
+                new BoxShadow(
+                  color: Colors.black,
+                  offset: new Offset(10.0, 5.0),
+                  blurRadius: 25.0,
+                )
+              ],
+              shape: BoxShape.circle,
+              color: Colors.blue[100 * ((index + 1) % 8)],
+              gradient: LinearGradient(
+                colors: [Colors.yellow, Colors.red],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+              image: DecorationImage(
+                image: AssetImage("assets/images/kedi.jpg"),
+                fit: BoxFit.contain,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+            margin: EdgeInsets.all(20),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Merhaba Flutter $index",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ),
+          ),
+          onTap: () => debugPrint("Merhaba flutter $index tıklanıldı"),
+          onDoubleTap: () =>
+              debugPrint("Merhaba flutter $index çift tıklanıldı"),
+          onLongPress: () => debugPrint("Merhaba flutter $index uzun basıldı"),
+          onHorizontalDragStart: (e) =>
+              debugPrint("Merhaba flutter $index uzun basıldı $e"),
+        );
+      },
+    );
+  }
+}
+
 GridView buildGridView_count() {
   return GridView.count(
     crossAxisCount: 2,
