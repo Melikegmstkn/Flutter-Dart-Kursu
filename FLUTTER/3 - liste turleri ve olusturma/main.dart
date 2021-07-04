dacc8cb8a98aa3bb70ce6fac5770e662024947d8
Melike <melikegmstkn@gmail.com>
2021-07-04 20:50:23 +0300

    listview/easyLoading

--- FLUTTER/3 - liste turleri ve olusturma/main.dart
+++ FLUTTER/3 - liste turleri ve olusturma/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 import 'card_listtile.dart';
 import 'custom_scroll_ve_slivers.dart';
+import 'listview_kullanimi.dart';
 
 void main() {
   runApp(MyApp());
 
+  configLoading();
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.yellow
+    ..backgroundColor = Colors.green
+    ..indicatorColor = Colors.yellow
+    ..textColor = Colors.white
+    ..maskColor = Colors.blue.withOpacity(0.5)
+    ..userInteractions = true
+    ..dismissOnTap = true;
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +32,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: CardveListtileKullanimi(),
+      home: ListViewKullanimi(),
+      builder: EasyLoading.init(),
     );
   }
 }
