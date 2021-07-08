24c44e443b3486018d6c5484aaa62150e4302b2a
Melike <melikegmstkn@gmail.com>
2021-07-08 14:49:14 +0300

    font kullanimi

---
+++ FLUTTER/6 - Tasarımı Kolaylaştırma/ozelFontKullanimi.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ozelFontKullanimi extends StatelessWidget {
+  const ozelFontKullanimi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Dersleri"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: Text(
+                "Genel Font Kullanimi",
+                style: TextStyle(
+                  fontFamily: 'GenelYaziStili',
+                  fontSize: 35,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Ozel Font Kullanimi",
+                style: TextStyle(
+                  fontFamily: 'OzelYaziStili',
+                  fontSize: 35,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Font Kullanimi",
+                style: TextStyle(
+                  //font family belirtilmedikçe themeData daki font kullanılır
+                  //fontFamily: 'OzelYaziStili',
+                  fontSize: 35,
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
