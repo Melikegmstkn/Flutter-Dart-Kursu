a0c70de0b45cefca810ba4cf0f5f3695e65e1dc3
Melike <melikegmstkn@gmail.com>
2021-07-10 18:34:34 +0300

    json convert ornek

--- FLUTTER/7 - Http paket json - Api/main.dart
+++ FLUTTER/7 - Http paket json - Api/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'local_json.dart';
+import 'remote_app.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    /*
+    Ogrenci melike = Ogrenci(21, "melike");
+    debugPrint(melike.toString());
+    Map<String, dynamic> nesliMap = {'id': 19, 'isim': "nesli"};
+    debugPrint(
+        "Adı:" + nesliMap['isim'] + " id: " + nesliMap['id'].toString());
+    Ogrenci n = Ogrenci.mapiNesneyeDonustur(nesliMap);
+    debugPrint(n.toString());
+    Map<String, dynamic> oznurMap = {'id': 45, 'isim': "öznur"};
+    Ogrenci o = Ogrenci.fromMap(oznurMap);
+    debugPrint(o.toString());
+    */
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -18,4 +32,21 @@ class MyApp extends StatelessWidget {
       home: LocalJsonKullanimi(),
     );
   }
+}
+
+class Ogrenci {
+  int id;
+  String isim;
+  Ogrenci(this.id, this.isim);
+  @override
+  String toString() {
+    return "Adı : $isim id: $id";
+  }
+
+  factory Ogrenci.mapiNesneyeDonustur(Map<String, dynamic> gelenMap) {
+    return Ogrenci(gelenMap['id'], gelenMap['isim']);
+  }
+  Ogrenci.fromMap(Map<String, dynamic> gelenMap)
+      : id = gelenMap['id'],
+        isim = gelenMap['isim'];
 }
