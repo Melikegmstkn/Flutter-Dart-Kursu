e3f070f1d676f797da6139aabe18143662e5a266
Melike <melikegmstkn@gmail.com>
2021-07-20 11:11:49 +0300

    providerle authentication

--- FLUTTER/11 - Provider  - block pattern/lib/main.dart
+++ FLUTTER/11 - Provider  - block pattern/lib/main.dart
@@ -1,6 +1,86 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'auth_service.dart';
+import 'counter.dart';
+import 'sayac_with_provider.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); //uygulama başlamadan çalışması gereken yapılar varsa
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+                color: Colors.blue,
+                child: Text("Provider ile Sayac App"),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => MultiProvider(
+                        providers: [
+                          ChangeNotifierProvider(
+                            create: (_) => Counter(5),
+                          ),
+                          ChangeNotifierProvider(
+                            create: (_) => AuthService(),
+                          ),
+                        ],
+                        child: ProviderlaSayacUygulamasi(),
+                      ),
+                    ),
+                  );
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//PROVİDER DERS 1 MAİN
+/*
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// import 'auth_service.dart';
 import 'counter.dart';
 import 'sayac_with_provider.dart';
 
@@ -65,3 +145,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
